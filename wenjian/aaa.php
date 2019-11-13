793b27d2d10ea1c2f5df10cfef99e15f7e6cdf49
suren8848 <2545486445@qq.com>
2019-11-13 19:40:54 +0800

    abc

--- wenjian/aaa.php
+++ wenjian/aaa.php
@@ -1,12 +1,30 @@
 <?php  
 	$arr=$_FILES;
+	$info=$_REQUEST;
+	$ext=explode(".",$info['filename'])[1];
+	$fileName=$info['filename'];
+
+	$baseDir="./".date("Y/m/d",time());
+	if(!is_dir($baseDir)){
+		mkdir($baseDir,0,777);
+	}
+	$filePath=$baseDir.$fileName;
+
+
+
 	$tmpName=$arr['data']['tmp_name'];
-	$fileName="111.jpg";
 	$content=file_get_contents($tmpName);
 	file_put_contents($fileName,$content,FILE_APPEND);
 
+	$filePath=ltrim($filePath,".");
+	$filePath="/file/".$filePath;
+
+
 	$arrRetuen=array(
-		"error"=>0
+		"error"=>0,
+		"data"=>array(
+			'path'=>$filePath,
+		),
 	);
 	echo json_encode($arrRetuen);
 ?>
